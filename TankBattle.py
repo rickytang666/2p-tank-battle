d00fa7627fc6f71c41340cc88866e8c532cb7caf
Ruiqi Tang <tangricky001@gmail.com>
2024-06-20 20:09:39 -0400

    Game objective

--- TankBattle.py
+++ TankBattle.py
@@ -1,8 +1,19 @@
 # Author: Ricky Tang
 # Created: June 6th, 2024
+# Finished: June 20th, 2024
 
-from GameTools import*
+#############################################################################################################
+#   GAME OBJECTIVE:
+#   The program is organized using classes which provide convenience
+#   A two-player tank battle game in a enclosed wall. 
+#   Each player used keyborad to control the tank to kill the enemy by taking away all the live points
+#   To help winning, the game offers 8 special techniques with bonus functionalities
+#   This game provides options to choose different screen sizes, suiting different kinds of computers
+#   The game has cool graphics, and have fireworks celebrating the winner at the end of the game
+#############################################################################################################
 
-TankBattle = Game()
+from GameTools import* # Importing the package
 
-TankBattle.runApplication()
+TankBattle = Game() # Object of the Game class
+
+TankBattle.runApplication() # Call the running function
